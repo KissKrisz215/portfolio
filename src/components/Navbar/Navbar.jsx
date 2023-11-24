09aaa2d2c58276e3ec365e8ad1a3ccf42d72f364
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-11-24 14:41:04 +0100

    Added Hero Component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import "./Navbar.css";
 import LanguageSelector from "../LanguageSelector/LanguageSelector";
-import { ReactComponent as FlagIcon } from "../../assets/hu.svg";
 
 function Navbar() {
   const [t, i18n] = useTranslation("global");
@@ -10,21 +9,21 @@ function Navbar() {
   return (
     <div className="navbar wrapper px-12">
       <div className="container flex items-center justify-between">
-        <a className="navbar-brand" href="">
+        <a className="navbar-brand" href="#home">
           Krisztian.dev
         </a>
         <div className="flex gap-7">
-          <a className="navbar-link" href="">
-            Home
+          <a className="navbar-link" href="#home">
+            {t("navbar.item1")}
           </a>
           <a className="navbar-link" href="">
-            About
+            {t("navbar.item2")}
           </a>
           <a className="navbar-link" href="">
-            Projects
+            {t("navbar.item3")}
           </a>
           <a className="navbar-link" href="">
-            Contact
+            {t("navbar.item4")}
           </a>
           <LanguageSelector />
         </div>
